bdfd06c2193b73537786cc41a565ae99e9b9e0ce
Seb Asus VS <sebas93cay@gmail.com>
2021-08-09 16:36:27 -0500

    Use update __dict__ instead of using kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -10,27 +10,18 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
         else:
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                self.updated_at = datetime.now()
-            if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                self.created_at = datetime.now()
-            if '__class__' in kwargs:
-                del kwargs['__class__']
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
             self.__dict__.update(kwargs)
-        from models import storage
-        storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
