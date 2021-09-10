c23f07a825eb0f748088faa13e672366bfc68531
Seb Asus VS <sebas93cay@gmail.com>
2021-09-10 11:38:03 -0500

    Task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from logging import debug
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
