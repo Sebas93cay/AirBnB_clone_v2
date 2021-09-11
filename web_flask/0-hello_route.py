86bae128988c61d69653cb6139a787473516b5e3
Seb Asus VS <sebas93cay@gmail.com>
2021-09-11 10:10:00 -0500

    Fix 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,9 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_world():
     """hello World"""
     return 'Hello HBNB!'
