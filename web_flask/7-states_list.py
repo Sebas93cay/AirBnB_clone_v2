19479ce7cd076c34aae3666a81b54de6d82bbdaa
Seb Asus VS <sebas93cay@gmail.com>
2021-09-11 10:51:45 -0500

    Task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,22 +3,24 @@
 This scripts tarts a Flask web application that shows a list of states
 """
 
-from flask import Flask
+from flask import Flask, g
 from flask.templating import render_template
 
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-# @app.teardown_appcontext
-# def close_session():
-    # storage.close()
+@app.teardown_appcontext
+def teardown_db(exeption):
+    storage.close()
+    # db = g.pop('db', None)
+    # if db is not None:
+    # db.close()
 
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def states_list():
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
