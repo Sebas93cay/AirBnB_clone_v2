b5caf42cabe2b1d7cfcf0fec9de78ef3bbf0e0f4
Seb Asus VS <sebas93cay@gmail.com>
2021-09-11 11:02:44 -0500

    Fix 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This scripts tarts a Flask web application that shows a list of states
+"""
+
+from flask import Flask, g
+from flask.templating import render_template
+
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exeption):
+    storage.close()
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
